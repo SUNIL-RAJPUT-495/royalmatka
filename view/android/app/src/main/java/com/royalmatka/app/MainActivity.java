7541ffd5a7001f1ee65c9213605538a1352b8a97
SUNIL-RAJPUT-495 <sunilsinghshekhawat495@gmail.com>
2026-08-29 17:02:13 +0530

    Create native Android NotificationChannel in MainActivity.java to register notification capabilities with Android OS

--- view/android/app/src/main/java/com/royalmatka/app/MainActivity.java
+++ view/android/app/src/main/java/com/royalmatka/app/MainActivity.java
@@ -1,5 +1,32 @@
 package com.royalmatka.app;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Default Notifications";
+            String description = "Game Result and Account Alerts";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel("default", name, importance);
+            channel.setDescription(description);
+            channel.enableVibration(true);
+            channel.enableLights(true);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+    }
+}
